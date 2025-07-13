55fcac50c6793a3d076bfca5d501bab93df1114f
nad <pan.tostado.ascii@gmail.com>
2025-07-13 18:07:59 -0500

    Initial state (Components)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './componets/footer/footer.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { HomeComponent } from './componets/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'footer', component: FooterComponent},
+    { path: 'navbar', component: NavbarComponent},
+    { path: 'home', component: HomeComponent}
+];
