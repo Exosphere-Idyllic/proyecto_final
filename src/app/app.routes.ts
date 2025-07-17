e7def8408b779f33a6a69594e82b8919d062f0bd
nad <pan.tostado.ascii@gmail.com>
2025-07-17 10:52:32 -0500

    add final version Contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { OffersComponent } from './components/offers/offers.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     { path: 'footer', component: FooterComponent},
     { path: 'navbar', component: NavbarComponent},
     { path: 'home', component: HomeComponent},
     { path: 'carousel', component: CarouselComponent},
-    { path: 'offers', component:OffersComponent}
+    { path: 'offers', component:OffersComponent},
+    { path: 'contact', component:ContactComponent}
 ];
